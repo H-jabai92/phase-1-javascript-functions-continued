51ba7bd3503e2765c2a3afc14a980168ed850ebb
Hasan <h.mjabai92@gmail.com>
2023-10-07 18:39:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(arg = 'roller-skate' ) {
+    return arg
+}
+ console.log(saturdayFun())
+
+function mondayWork(arg = "go to the office") {
+    return arg 
+     
+}
+console.log(mondayWork())
+ 
